4e6057a2f5c12860637e42bb95a1250ac94023f3
Magno Carvalho <magnocarv@hotmail.com>
2018-03-18 01:36:12 -0300

    Initial commit
    primeiro commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// var app = require ('./config/express')();
+// var rotasproduto = require('.app/routes/produtos')(app);
+// app.listen(3000,function(){
+//     console.log("servidor rodando")
+// });
+// var express = require('./config/express')();
+// var app = configura();
+
+// app.set('view engine','ejs'); 
+// app.get('/produtos',function(req,res)
+// {
+//     res.render("produtos/lista")
+// })
+
+// app.listen(3000, function(){
+//     console.log("servidoo 100%");
+// })
+
+var app = require('./config/express')();
+var rotasProdutos = require('./app/routes/produtos')(app);
+
+    console.log("Servidor rodando");
+});
